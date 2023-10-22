af11f118d455453bbe151b88024cb2b3113b29b3
Beetlejuice7 <pragyanstha1@gmail.com>
2023-10-22 19:52:29 +0545

    edit function completed

--- SMS.cpp
+++ SMS.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<string.h>
 #include<stdlib.h>
 #include<stdio.h>
 #include<iomanip>
+#include<stdbool.h>
 using namespace std;
 
-fstream file;
+fstream file, file1;
 
 //1
 void student();
@@ -16,6 +18,7 @@ void result();
 //2
 void addstd();
 void showstd();
+void editstd();
 
 struct student
 {
@@ -95,7 +98,7 @@ void student()
         break;
 
     case 3 :
-        cout <<"hello";
+        editstd();
         break;
 
     case 4 :
@@ -156,6 +159,189 @@ void showstd()
         total += 1;
     }
     file.close();
+    cout << "Press enter to go back to menu..." << endl;
+    cin.ignore();
+    cin.get();
+    return student();
+}
+/*void editstd()
+{
+    system("clear");
+    cout << "-----------STUDENT MANAGEMENT SYSTEM--------------" << endl << endl;
+    int id;
+    bool found=false;
+    file.open("std.txt", ios :: in);
+    if (!file)
+    {
+        cout << "No data available." << endl;
+        file.close();
+    }
+    else
+    {
+        cout << "Enter id of the student to modify : ";
+        cin >> id;
+        file1.open("temp.txt", ios :: app | ios :: out);
+        file >> std1.id >> std1.name >> std1.age >> std1.faculty;
+        while (!file.eof())
+        {
+            if (id != std1.id)
+            {
+                file1 << " " << std1.id << " " << std1.name<< " " << std1.age << " " << std1.faculty << "\n";
+            }
+            else
+            {
+                cout << "Found student with ID : " << std1.id << endl;
+                cout << "Name : " << std1.name << endl;
+                cout << "Age : " << std1.age << endl;
+                cout << "Faculty : " << std1.faculty << endl << endl;
+                //ask user for modified information
+                cout << "Enter name : ";
+                cin >> std1.name;
+                cout << "Enter age : ";
+                cin >> std1.age;
+                cout << "Enter faculty : ";
+                cin >> std1.faculty;
+                file1 << std1.id << std1.name << std1.age << std1.faculty << endl;
+                found = true;
+            }
+            
+        }
+        file.close();
+        file1.close();
+        //remove orginal file and rename temporary file
+        remove("std.txt");
+        rename("temp.txt", "std.txt");
+
+        if (!found)
+        {
+            cout << "No student found with ID : " << std1.id << endl;
+        }
+        else
+        {
+            cout << "Student information modified sucessfulluy." << endl;
+        }
+        cout << "Press enter to go back to menu..." << endl;
+        cin.ignore();
+        cin.get();
+        return student();
+        
+
+    }
+}*/
+/*void editstd()
+{
+    system("clear");
+    cout << "-----------STUDENT MANAGEMENT SYSTEM--------------" << endl << endl;
+    int id;
+    bool found = false;
+    cout << "Enter ID of student to modify : ";
+    cin >> id;
+    file.open("std.txt", ios :: in);
+    file1.open("temp.txt", ios :: out);
+
+        while (file1 >> std1.id >> std1.name >> std1.age >> std1.faculty)
+    {
+        if (id == std1.id)
+        {
+            found = true;
+            cout << "Found student with ID : " << std1.id << endl;
+            cout << "Name : " << std1.name << endl;
+            cout << "Age : " << std1.age << endl;
+            cout << "Faculty : " << std1.faculty << endl << endl;
+            //ask user to modify information
+            cout << "Enter name : ";
+            cin >> std1.name;
+            cout << "Enter age : ";
+            cin >> std1.age;
+            cout << "Enter faculty : ";
+            cin >> std1.faculty;
+            file1 << std1.id << std1.name << std1.age << std1.faculty << endl;
+        }
+        else
+        {
+            file1 << std1.id << std1.name << std1.age << std1.faculty << endl;
+        }
+    }
+    file.close();
+    file1.close();
+    //remove original file and rename temporary file
+    remove("std.txt");
+    rename("temp.txt", "std.txt");
+
+    if (!found)
+    {
+        cout << "No student found with ID : " << std1.id << endl; 
+    }
+    else
+    {
+        cout << "Student information modified successfully." << endl;
+    }
+    cout << "Press enter to go back to menu...";
+    cin.ignore();
+    cin.get();
+    return student();
+    
+}*/
+void editstd()
+{
+    system("clear");
+    cout << "-----------STUDENT MANAGEMENT SYSTEM--------------" << endl << endl;
+    int id;
+    bool found = false;
+    cout << "Enter ID of student to modify: ";
+    cin >> id;
+
+    file.open("std.txt", ios::in);
+    file1.open("temp.txt", ios::out);
+
+    while (file >> std1.id >> std1.name >> std1.age >> std1.faculty)
+    {
+        if (id == std1.id)
+        {
+            found = true;
+            // Modify the student information here if needed
+            // For example, you can allow the user to edit name, age, or faculty
+            // Then write the modified information to the temporary file
+            // For now, let's just display the existing information for editing
+            cout << "Found student with ID: " << std1.id << endl;
+            cout << "Name: " << std1.name << endl;
+            cout << "Age: " << std1.age << endl;
+            cout << "Faculty: " << std1.faculty << endl;
+
+            // Ask user for modified information
+            cout << "Enter modified name: ";
+            cin >> std1.name;
+            cout << "Enter modified age: ";
+            cin >> std1.age;
+            cout << "Enter modified faculty: ";
+            cin >> std1.faculty;
+
+            // Write modified information to temporary file
+            file1 << std1.id << " " << std1.name << " " << std1.age << " " << std1.faculty << endl;
+        }
+        else
+        {
+            // If the ID does not match, write the information as it is to the temporary file
+            file1 << std1.id << " " << std1.name << " " << std1.age << " " << std1.faculty << endl;
+        }
+    }
+
+    file.close();
+    file1.close();
+
+    // Remove the original file and rename the temporary file
+    remove("std.txt");
+    rename("temp.txt", "std.txt");
+
+    if (!found)
+    {
+        cout << "No student found with ID: " << id << endl;
+    }
+    else
+    {
+        cout << "Student information modified successfully!" << endl;
+    }
+
     cout << "Press enter to go back to menu..." << endl;
     cin.ignore();
     cin.get();
